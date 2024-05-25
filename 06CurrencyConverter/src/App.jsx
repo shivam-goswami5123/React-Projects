1e424d44fbc78dfe59e8e9b53746c42c16f0a82b
shivam-goswami5123 <shivam.goswami.ug22@nsut.ac.in>
2024-05-25 19:25:00 +0530

    06CurrencyConverter Setup Files

---
+++ 06CurrencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500'>CURRENCY CONVERTER</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 06CurrencyConverter/src/components/InputBox.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function input(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default input;
