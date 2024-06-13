3591d38d3933b5ab62571917a84c22e63ffd5a0c
shivam-goswami5123 <shivam.goswami.ug22@nsut.ac.in>
2024-06-13 23:19:36 +0530

    12BlogApp Redux Add

--- 12BlogAppAppwrite/src/App.jsx
+++ 12BlogAppAppwrite/src/App.jsx
@@ -1,14 +1,43 @@
 
+import React,{ useState ,useEffect } from 'react'
 import './App.css'
+import {useDispatch} from "react-redux"
+import authService from "./appwrite/auth"
+import { login, logout } from './features/authSlice'
+import { Footer, Header } from './components'
+import {Outlet} from "react-router-dom"
 
 function App() {
-  
+  const [loading , setLoading] = useState(true)
+  const dispatch=useDispatch()
 
-  return (
-    <>
-      <h1>Welcome to BloggerBlog</h1>
-    </>
-  )
-}
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .catch((error)=>{ console.log("ERR IN LOADING:",error.message)})
+    .finally(()=>setLoading(false))
+  },[])
+
+
+
+  return !loading ? (
+    <div className="bg-slate-800  w-full h-full">
+      <div className="text-gray-200 w-full h-full">
+        <Header/>
+        <main>
+        TODO:<Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ): null
+  
+} 
 
 export default App
